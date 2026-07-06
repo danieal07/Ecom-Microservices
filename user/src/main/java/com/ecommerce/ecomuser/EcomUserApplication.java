69bda98b1c5ebb3426984bbbf28da173959d335c
danieal <daniealantony@gmail.com>
2026-07-06 16:34:27 +0530

    the user , the product and the  the order services have been added

---
+++ user/src/main/java/com/ecommerce/ecomuser/EcomUserApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecomuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomUserApplication.class, args);
+    }
+
+}
---
+++ user/src/main/java/com/ecommerce/ecomuser/model/UserRole.java
@@ -0,0 +1,5 @@
+package com.ecommerce.ecomuser.model;
+
+public enum UserRole {
+    CUSTOMER, ADMIN
+}
